9b0ebb4e80e774cae41d3b363668d468f9bee685
AtharvKadam0807 <81708451+AtharvKadam0807@users.noreply.github.com>
2026-05-21 20:57:07 +0200

    Some update with app.py

--- app.py
+++ app.py
@@ -7,12 +7,21 @@ app=Flask(__name__)
 
 #Load the Model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
+scaling=pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict', method=['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
     data=request.json['data']
     print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scaling.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True)
